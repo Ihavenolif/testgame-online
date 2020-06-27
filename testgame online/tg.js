dc63c5434400aa6e6e4514d331d5b2fcac439090
matus wall <matuswall@gmail.com>
2020-06-27 17:12:32 +0200

    custom connection

--- testgame online/tg.js
+++ testgame online/tg.js
@@ -1,3 +1,4 @@
+ip = ""
 playerName = ""
 gameName = ""
 password = ""
@@ -17,7 +18,7 @@ function refresh() {
             row = table.insertRow(i++)
             row.insertCell(ii++).innerHTML = x
             row.insertCell(ii++).innerHTML = gamesList[x]["player1"]
-            row.insertCell(ii++).innerHTML = gamesList[x]["password"] == "" ? "no" : "yes"
+            row.insertCell(ii++).innerHTML = gamesList[x]["password"]
             row.insertCell(ii).innerHTML = "<button class=joinButton onclick=joinGame(" + "gamesList[" + "'" + x + "'" + "])>Join</button>"
         }
     }, 500);
@@ -42,7 +43,7 @@ function getGamesList() {
             gamesList = JSON.parse(this.responseText)
         }
     }
-    xhttp.open("POST", "http://localhost:7000", true)
+    xhttp.open("POST", ip, true)
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
     xhttp.send(JSON.stringify({
         getGamesList: true
@@ -50,7 +51,8 @@ function getGamesList() {
 }
 
 function createGame(gameName, password) {
-    y = ""
+    // REPLACING " " WITH "-"
+    y = "" 
     for (x of gameName) {
         if (x == " ") {
             y += "-"
@@ -65,7 +67,7 @@ function createGame(gameName, password) {
             console.log(typeof this.responseText == Object ? this.responseText : JSON.parse(this.responseText))
         }
     }
-    xhttp.open("POST", "http://localhost:7000", true)
+    xhttp.open("POST", ip, true)
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
     xhttp.send(JSON.stringify({
         createGame: true,
@@ -73,7 +75,7 @@ function createGame(gameName, password) {
         password: password,
         playerName: playerName
     }))
-    lobbySelection()
+    gameLobby(playerName, "Waiting for another player", 1)
 }
 
 function lobbySelection() {
@@ -81,8 +83,12 @@ function lobbySelection() {
     refresh()
 }
 
-function gameLobby() {
-    document.getElementById("content").innerHTML = ""
+function gameLobby(name1, name2, player) {
+    if (player == 1) {
+        document.getElementById("content").innerHTML = "<div class=login-box><h1>Game lobby</h1><div class=dual><div class=halfFlex><span id=name1>" + name1 + "</span><button class=login-btn>Ready</button></div><div class=halfFlex><span id=name2>" + name2 + "</span><br><img class=readyImg src=notReady.png></div><div></div>"
+    } else if (player == 2) {
+        
+    }
 }
 
 function createGameMenu() {
@@ -103,7 +109,7 @@ function joinGame(gameId) {
                 //gamesList = JSON.parse(this.responseText)
             }
         }
-        xhttp.open("POST", "http://localhost:7000", true)
+        xhttp.open("POST", ip, true)
         xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
         xhttp.send(JSON.stringify({
             joinGame: true,
@@ -115,4 +121,65 @@ function joinGame(gameId) {
     } else {
         alert("You cannot connect to yourself.")
     }
+}
+
+function connectOfficial(){
+    connectionSuccessful = false
+    document.getElementById("content").innerHTML = "<div class=login-box><h1>Connecting...</h1></div>"
+    xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText)
+            document.getElementById("content").innerHTML = "<div class=login-box><h1>TESTGAME ONLINE</h1><input class=textbox placeholder=Nickname id=nick><br><button class=login-btn onclick=login()>Login</button></div>"
+            connectionSuccessful = true
+            ip = "https://testgame-server.herokuapp.com/"
+        }
+    }
+    xhttp.open("POST", "https://testgame-server.herokuapp.com/", true)
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+    xhttp.send(JSON.stringify({
+        checkConnection: true
+    }))
+
+    setTimeout(() => {
+        if(!connectionSuccessful){
+            alert("Connection failed")
+            document.getElementById("content").innerHTML = "<div class=login-box><h1>Choose a server</h1><div class=dual><div class=halfflex><button class=chooseOneBtn onclick=connectOfficial()>Connect to official servers</button></div><div class=halfflex><button class=chooseOneBtn onclick=connectCustom()>Connect to a custom server</button></div></div></div>"
+        }
+    }, 10000);
+}
+
+function connectCustom(){
+    if(document.getElementById("ipCache").value.startsWith("localhost")){
+        cacheIP = "http://" + document.getElementById("ipCache").value
+    } else{
+        cacheIP = "https://" + document.getElementById("ipCache").value 
+    }
+    connectionSuccessful = false
+    document.getElementById("content").innerHTML = "<div class=login-box><h1>Connecting...</h1></div>"
+    xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText)
+            document.getElementById("content").innerHTML = "<div class=login-box><h1>TESTGAME ONLINE</h1><input class=textbox placeholder=Nickname id=nick><br><button class=login-btn onclick=login()>Login</button></div>"
+            connectionSuccessful = true
+            ip = cacheIP
+        }
+    }
+    xhttp.open("POST", cacheIP, true)
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+    xhttp.send(JSON.stringify({
+        checkConnection: true
+    }))
+
+    setTimeout(() => {
+        if(!connectionSuccessful){
+            alert("Connection failed")
+            document.getElementById("content").innerHTML = "<div class=login-box><h1>Choose a server</h1><div class=dual><div class=halfflex><button class=chooseOneBtn onclick=connectOfficial()>Connect to official servers</button></div><div class=halfflex><button class=chooseOneBtn onclick=connectCustom()>Connect to a custom server</button></div></div></div>"
+        }
+    }, 10000);
+}
+
+function customConnectionSettings(){
+    document.getElementById("content").innerHTML = "<div class=login-box><h1>Custom server IP</h1><br><input class=textbox placeholder=IP id=ipCache><br><button class=login-btn onclick=connectCustom()>Connect</button></div>"
 }
